31dc250b1f3968fdf7c8f7e58f406f27925b1798
md.munna <mk8761174@gmail.com>
2025-09-21 21:15:47 +0600

    comparison practice done

--- comparison.js
+++ comparison.js
@@ -11,3 +11,17 @@
  * 8.or: || 
  * 
  */
+
+// console.log(5 > 10)
+// console.log(5 < 10)
+// console.log(10 >= 10)
+// console.log(15 >= 10)
+
+
+// var peyaraPrice = 100
+// var applePrice = 300
+// console.log(peyaraPrice < applePrice)
+
+console.log( 10 == "10")
+//============ triple === value and type check
+console.log( 10 === "10")
